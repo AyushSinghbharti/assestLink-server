2d15cf10fc401e478451ecc1c85f7e67a177553f
Ayush Singh <ayushsinghbharti123@gmail.com>
2025-08-11 11:17:50 +0530

    Temp fix

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Direct route for assetlinks.json
+app.get("/.well-known/assetlinks.json", (req, res) => {
+  res.sendFile(path.join(__dirname, ".well-known", "assetlinks.json"));
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server running...");
 });
